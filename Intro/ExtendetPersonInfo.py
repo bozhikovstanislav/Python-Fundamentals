fd7405b83c00ccfd998e6f954288f61371c8f67f
stanislav bozhikov <bozhikov.stanislav@gmail.com>
2019-01-09 22:10:41 +0200

    lectures presentations

---
+++ Intro/ExtendetPersonInfo.py
@@ -0,0 +1,12 @@
+name = input()
+age = int(input())
+town = input()
+salery = float(input())
+age_range = input()
+salery_range = input()
+
+
+extendetinfo = 'name: ' + f'{name}' + '\n' + 'age: ' + f'{age}' + '\n' + 'town: ' + f'{town}' + '\n' + 'salery: ' + f'${salery}' + '\n' + 'Age range: ' + f'{age_range}' + '\n' + 'Salery range: ' + f'{salery_range}'
+
+
+print(extendetinfo)
