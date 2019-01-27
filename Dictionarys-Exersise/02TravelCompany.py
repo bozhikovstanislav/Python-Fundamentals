c736d4144a1dd662a851ede0223754218e913cbb
stanislav bozhikov <bozhikov.stanislav@gmail.com>
2019-01-27 14:40:08 +0200

    03 dic ref advanced

--- Dictionarys-Exersise/02TravelCompany.py
+++ Dictionarys-Exersise/02TravelCompany.py
@@ -1,9 +1,7 @@
-transport_capacity_dict = {}
 transport_capacity_list = []
-
 travel_dict = {}
 data = input()
-while not data == 'ready':
+while data.lower() != 'ready'.lower():
     city_data = data.split(':')[0]
     transport_capacity_list = data.split(':')[1:][0].split(',')
     if city_data not in travel_dict:
@@ -11,13 +9,28 @@ while not data == 'ready':
         for x in range(len(transport_capacity_list)):
             type_travel = transport_capacity_list[x].split('-')[0]
             capacity_travel = transport_capacity_list[x].split('-')[1]
-            travel_dict[city_data][type_travel] = capacity_travel
+            travel_dict[city_data][type_travel] = int(capacity_travel)
     elif city_data in travel_dict.keys():
         for x in range(len(transport_capacity_list)):
             type_travel = transport_capacity_list[x].split('-')[0]
             capacity_travel = transport_capacity_list[x].split('-')[1]
             if type_travel in travel_dict[city_data].keys():
-                travel_dict[city_data][type_travel] = capacity_travel
+                travel_dict[city_data][type_travel] = int(capacity_travel)
     data = input()
 
-print(travel_dict)
+data_travel = input()
+
+while data_travel.lower() != 'travel time!'.lower():
+    group_to_travel = data_travel.split(' ')
+    city_to_go = group_to_travel[0]
+    number_of_people = int(group_to_travel[1])
+    seats = 0
+    for x, v in travel_dict.items():
+        if city_to_go in x:
+            for xw, f in v.items():
+                seats += int(f)
+    if number_of_people < seats:
+        print(f'{city_to_go} -> all {number_of_people} accommodated')
+    elif number_of_people > seats:
+        print(f'{city_to_go} -> all except {number_of_people - seats} accommodated')
+    data_travel = input()
