ede4b371fd9acb3613660d354b9835cb2de58fee
stanislav <bozhikov.stanislav@gmail.com>
2019-02-03 19:23:06 +0200

    String Commander 08

--- Strings/08.py
+++ Strings/08.py
@@ -14,3 +14,8 @@ while True:
 for key, value in sorted(char_dict.items()):
     for v1 in value:
         print(key)
+
+#string = input()
+
+#for char in sorted(set(string), key=string.index):
+#    print(char + ":" + ("/".join(map(str, [i for i, ltr in enumerate(string) if ltr == char]))))
